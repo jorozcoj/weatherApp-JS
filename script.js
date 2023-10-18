78a3f610393170a293680827674420abde015a5a
jhorman Orozco <j.orozco.j23@gmail.com>
2023-10-18 15:59:04 -0500

    Se agregan estilos al proyecto

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let apiKey = "bef63a4620e5dbc0cecbc4cf042a7bbe";
+const apiKey = "bef63a4620e5dbc0cecbc4cf042a7bbe";
 const difKelvin = 273.15;
+const container = document.getElementById("contenedor");
+const weatherBox = document.querySelector("weather-box");
+const weaterDetails = document.querySelector("weather-details");
+const searchButton = document.getElementById("botonBusqueda");
 
-document.getElementById("botonBusqueda").addEventListener("click", () => {
+searchButton.addEventListener("click", () => {
   const city = document.getElementById("ciudadEntrada").value;
   if (city) {
     fetchDatosClima(city);
@@ -17,34 +21,47 @@ function fetchDatosClima(city) {
 }
 
 function mostrarDatosClima(data) {
-  const divdatosClima = document.getElementById("datosClima");
-  divdatosClima.innerHTML = "";
-  const cityName = data.name;
-  const temperature = data.main.temp;
+  const image = document.querySelector(".weather-box img");
+  const temperature = document.querySelector(".weather-box .temperature");
+  const description = document.querySelector(".weather-box .description");
+  const humidity = document.querySelector(".weather-details .humidity span");
+  const wind = document.querySelector(".weather-details .wind span" );
+
+  switch (data.weather[0].main) {
+    case "Clear":
+      image.src = "images/clear.png";
+      break;
+    case "Rain":
+      image.src = "images/rain.png";
+      break;
+    case "Cloud":
+      image.src = "images/cloud.png";
+      break;
+    case "Snow":
+      image.src = "images/snow.png";
+      break;
+    case "Mist":
+      image.src = "images/mist.png";
+      break;
+    case "Haze":
+      image.src = "images/mist.png";
+      break;
+
+    default:
+      image.src = "images/cloud.png";
+      break;
+  }
+
+  const t = parseInt(Math.floor((data.main.temp)-difKelvin))
   const condition = data.weather[0].main;
+  const windSpeed = parseInt(data.wind.speed)
   const humity = data.main.humidity;
-  const icon = data.weather[0].icon;
+  const descr = data.weather[0].description;
 
-  const cityTitle = document.createElement("h2");
-  cityTitle.textContent = cityName;
+  
+temperature.innerHTML=`${t}<span>°C</span>`;
+description.innerHTML = descr;
+humidity.innerHTML = `${humity}%`;
+wind.innerHTML =  `${windSpeed}Km/h`;
 
-  const tempTitle = document.createElement("p");
-  tempTitle.textContent = `la temperatura es de ${Math.floor(
-    temperature - difKelvin
-  )} °C`;
-
-  const humityTitle = document.createElement("p");
-  cityTitle.textContent = `La humedad es de ${humity} %`;
-
-  const iconInfo = document.createElement("img");
-  iconInfo.src=`https://openweathermap.org/img/wn/${icon}@2x.png`;
-
-  const conditionTitle = document.createElement("p");
-  conditionTitle.textContent = `Las condiciones climatologicas son ${condition}`;
-
-  divdatosClima.appendChild(cityTitle);
-  divdatosClima.appendChild(tempTitle);
-  divdatosClima.appendChild(humityTitle)
-  divdatosClima.appendChild(conditionTitle);
-  divdatosClima.appendChild(iconInfo)
 }
